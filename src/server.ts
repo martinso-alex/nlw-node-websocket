42b62a293dfedac8b5c44785286d4fd609344205
Alexandre Martins <martinso.alex@gmail.com>
2021-04-20 14:27:58 -0300

    typeorm e arquitetura definidos

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import express from "express"
 
+import "./database"
+import { routes } from "./routes"
+
 const app = express()
 
-app.get('/', (req, res) => res.send('hello nlw#5'))
+app.use(express.json())
+
+app.use(routes)
 
 app.listen(3333, () => console.log('server is running on port 3333'))
